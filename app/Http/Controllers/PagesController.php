04f50b2d96b5557db4732f3b38bb509ab3169c3c
Synaxis <01xtor@gmail.com>
2018-02-26 23:37:18 -0300

    Blade Templating & Compiling Assets

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -7,6 +7,19 @@ use Illuminate\Http\Request;
 class PagesController extends Controller
 {
     public function index(){
-        return view ('pages.index');
+        $title = 'Bem vindo ao laravel';
+        return view ('pages.index')->with('title', $title);
+    }
+
+    public function about(){
+        return view('pages.about');
+    }
+
+    public function services(){
+        $data = array(
+            'title' => 'Services',
+            'services' => ['Crazy guy', 'Plays bfh', 'wtf']
+        );
+        return view('pages.services')->with($data);
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -18,8 +18,8 @@
 
 Route::get('/', 'PagesController@index');    
 
-Route::get('/about', function(){
-    return view('pages.about');
-});
+Route::get('/about', 'PagesController@about');
+
+Route::get('/services', 'PagesController@services');    
 
 
